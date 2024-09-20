3b2c1beef9bfcaf836688b38b29e09f826388df2
jenglish12 <jenglish12@mail.pima.edu>
2024-09-20 13:56:57 -0700

    Update cis129_lab03_coffeeShop.py
    Psuedocode to code, now I got to sort out all the bugs

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,1 +1,39 @@
+print ('***************************************\n
+My Coffee and Muffin Shop\n
+Number of coffees bought?')
 
+numberCoffees = input ()
+
+#Combines two steps by printing something while asking for an input
+numberMuffins = input ('Number of muffins bought?')
+
+#Goal is to prevent ordering one coffees or two coffee
+if numberCoffees == 1:
+  pluralCoffee = "s"
+else
+  pluralCoffee = ""
+
+if numberMuffins == 1:
+  pluralMuffin = "s"
+else
+  pluralMuffin = ""
+
+#Total price to pay
+coffeeCost = numberCoffees * 5
+muffinCost = numberMuffins * 5
+subtotal = coffeeCost + muffinCost
+tax = subtotal * 0.06
+total = subtotal + tax
+
+print ('***************************************\n
+***************************************\n
+My Coffee and Muffin Shop Receipt\n'
+numberCoffees + ' Coffee' + pluralCoffee + ' at $5 each: $ ' + coffeeCost + '\n'
+numberMuffins + ' Muffin' + pluralMuffin + ' at $4 each: $ ' + muffinCost + '\n'
+'6% tax: $ ' + tax + '\n
+--------- \n
+Total: $ ' + total + '\n
+***************************************\n
+Have a great day!\n
+\n
+***************************************')
